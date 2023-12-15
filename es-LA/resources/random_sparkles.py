8ff52225730c57bae8d4adc90935559885867f52
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2023-12-15 16:47:12 +0000

    New translations random_sparkles.py (Spanish, Latin America)

--- es-LA/resources/random_sparkles.py
+++ es-LA/resources/random_sparkles.py
@@ -2,7 +2,7 @@ from sense_hat import SenseHat
 from random import randint
 from time import sleep
 
-sense = SenseHat()
+sensor = SenseHat()
 
 while True:
     x = randint(0, 7)
@@ -10,5 +10,5 @@ while True:
     r = randint(0, 255)
     g = randint(0, 255)
     b = randint(0, 255)
-    sense.set_pixel(x, y, r, g, b)
+    sensor.set_pixel(x, y, r, g, b)
     sleep(0.01)
